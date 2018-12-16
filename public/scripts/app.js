a31ce3c33fa3e248001fe0a8f6006dcadeb25281
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-12-16 15:47:34 +0000

    JSX arrays - generate JSX elements by pushing to an array

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,97 +2,84 @@
 
 console.log('App.js is running!');
 
-// Challenge:
-// only render subtitle (and p tag) if subtitle exists - logical AND
-// render new p tag - if options.length > 0 print "Here are your options", otherwise "No options"
-
 var app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
-};
-
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  app.options && app.options.length > 0 ? "Here are your options" : "No options",
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item one'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item two'
-    )
-  )
-);
-
-var counter = 0;
-// console would show counter incrementing but <h1> below doesn't change
-// This is because React compiles the template resolving expressions (variables & functions)
-// and renders it to the DOM
-// Updating the variable alone isn't enough - we need to recomple the template + re-render 
-var addOne = function addOne() {
-  counter++;
-  console.log('addOne:' + counter);
-  renderCounterApp();
-};
-var minusOne = function minusOne() {
-  counter--;
-  console.log('minusOne:' + counter);
-  renderCounterApp();
-};
-var reset = function reset() {
-  counter = 0;
-  console.log('reset:' + counter);
-  renderCounterApp();
+  options: []
 };
 
 var appRoot = document.getElementById('app');
 
-var renderCounterApp = function renderCounterApp() {
-  var templateTwo = React.createElement(
+var onRemoveAll = function onRemoveAll() {
+  app.options.length = 0;
+  render();
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(e.target);
+  console.log(e.target.elements.option.value);
+
+  var option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(e.target.elements.option.value);
+    e.target.elements.option.value = '';
+
+    render();
+  }
+};
+
+// const numbers = [5, 67, 700];
+
+var render = function render() {
+  var template = React.createElement(
     'div',
     null,
     React.createElement(
       'h1',
       null,
-      'Count: ',
-      counter
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    app.options && app.options.length > 0 ? "Here are your options" : "No options",
+    React.createElement(
+      'p',
+      null,
+      app.options.length
     ),
     React.createElement(
       'button',
-      { onClick: addOne },
-      '+1'
+      { onClick: onRemoveAll },
+      'Remove All'
     ),
     React.createElement(
-      'button',
-      { onClick: minusOne },
-      '-1'
+      'ol',
+      null,
+      app.options.map(function (option) {
+        return React.createElement(
+          'li',
+          { key: option },
+          option
+        );
+      })
     ),
     React.createElement(
-      'button',
-      { onClick: reset },
-      'Reset'
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
     )
   );
 
-  ReactDOM.render(templateTwo, appRoot);
+  ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+render();
--- src/app.js
+++ src/app.js
@@ -1,61 +1,63 @@
 console.log('App.js is running!');
 
-// Challenge:
-// only render subtitle (and p tag) if subtitle exists - logical AND
-// render new p tag - if options.length > 0 print "Here are your options", otherwise "No options"
-
 const app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
-};
-
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    {(app.options && app.options.length > 0) ? "Here are your options" : "No options"}
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
-
-let counter = 0;
-// console would show counter incrementing but <h1> below doesn't change
-// This is because React compiles the template resolving expressions (variables & functions)
-// and renders it to the DOM
-// Updating the variable alone isn't enough - we need to recomple the template + re-render 
-const addOne = () => {
-  counter++;
-  console.log('addOne:' + counter);
-  renderCounterApp();
-}
-const minusOne = () => {
-  counter--;
-  console.log('minusOne:' + counter);
-  renderCounterApp();
-}
-const reset = () => {
-  counter = 0;
-  console.log('reset:' + counter);
-  renderCounterApp();
+  options: []
 };
 
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-  const templateTwo = (
+const onRemoveAll = () => {
+  app.options.length = 0;
+  render();
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target);
+  console.log(e.target.elements.option.value);
+
+  const option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(e.target.elements.option.value);
+    e.target.elements.option.value = '';
+
+    render();
+  }
+};
+
+// const numbers = [5, 67, 700];
+
+const render = () => {
+  const template = (
     <div>
-      <h1>Count: {counter}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      {(app.options && app.options.length > 0) ? "Here are your options" : "No options"}
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      {/* {[<p>a</p>, <p>b</p>, <p>c</p>]} */}
+      {/* {
+        numbers.map(number => {
+          return <p key={number}>Number: {number}</p>
+        })
+      } */}
+      <ol>
+        {
+          app.options.map(option => {
+            return <li key={option}>{option}</li>
+          })
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     </div>
   );
 
-  ReactDOM.render(templateTwo, appRoot);
+  ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+render();
