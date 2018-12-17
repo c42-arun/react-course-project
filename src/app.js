d4901b5513095c9d29513c7a99093240efecb83c
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-12-17 21:36:47 +0000

    Attach 'this' to event handler at constructor -  one time and efficient

--- src/app.js
+++ src/app.js
@@ -35,6 +35,10 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
   handleRemoveAll() {
     // alert('handleRemoveAll');
     console.log(this.props.options);
@@ -43,7 +47,7 @@ class Options extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={this.handleRemoveAll.bind(this)}>Remove All</button>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {this.props.options.map((option) => <Option key={option} optionText={option}/>)}
       </div>
     );
