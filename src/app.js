472c019278318abbf8b1cd7f59210ec3aef7d8c6
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-12-25 16:18:35 +0000

    React via npm

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-import seniorFunc, { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js built using webpack is running!');
+const template = React.createElement('p', {}, 'Component through npm package');
 
-console.log(18, 'An adult?', isAdult(18));
-console.log(17, 'An adult?', isAdult(17));
-
-console.log(18, 'Can drink?', canDrink(18));
-console.log(25, 'Can drink?', canDrink(25));
-
-console.log(64, 'Is senior?', seniorFunc(64));
-console.log(65, 'Is senior?', seniorFunc(65));
+ReactDOM.render(template, document.getElementById('app'));
