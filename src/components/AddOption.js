aa8ff31e186870ef6ba235c99f686c48aa13efb7
Arunkumar Ramachandriya <arunkumar@hawesandcurtis.com>
2018-12-27 15:51:58 +0000

    First component moved to its own file!

---
+++ src/components/AddOption.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default class AddOption extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        error: undefined
+      };
+  
+      this.handleAddOption = this.handleAddOption.bind(this);
+    }
+    handleAddOption(e) {
+      e.preventDefault();
+      const option = e.target.elements.option.value.trim();
+  
+      if (option) {
+        const error = this.props.handleAddOption(option);
+  
+        if (!error) {
+          e.target.elements.option.value = '';
+        }
+        
+        this.setState(() => ({ error }));
+      }
+    }
+  
+    render() {
+      return (
+        <div>
+          {this.state.error && <div>{this.state.error}</div>}
+          <form onSubmit={this.handleAddOption}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+          </form>
+        </div>
+      );
+    }
+  }
