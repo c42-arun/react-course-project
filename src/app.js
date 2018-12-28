82ed9f1b5b2a1112db0abc9854e3ae073dffa2a6
Arunkumar Ramachandriya <arunkumar@hawesandcurtis.com>
2018-12-28 13:21:54 +0000

    ES6 class property enabled - via Babel plugin & code demo; factored out dev dependencies

--- src/app.js
+++ src/app.js
@@ -4,3 +4,34 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
  
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+    constructor() {
+        this.name = 'Arun';
+        this.greeting = this.greeting.bind(this);
+    }
+
+    greeting() {
+        console.log(`Hi from ${this.name}`);
+    }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+const myOldGreet = oldSyntax.greeting;
+myOldGreet();
+
+class NewSyntax {
+    name = 'Rishi';
+
+    greeting = () => {
+        console.log(`Hi from ${this.name}`);
+    }
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
+
+const myNewGreet = newSyntax.greeting;
+myNewGreet();
