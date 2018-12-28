76e39c78ddabed9f5bf6221d82062f4b4c03beeb
Arunkumar Ramachandriya <arunkumar@hawesandcurtis.com>
2018-12-28 15:09:03 +0000

    Round 2: Injecting markup using 'props.children' and in-line HTML

--- src/app.js
+++ src/app.js
@@ -2,24 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
- 
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
 const Layout = (props) => {
     return (
         <div>
             <p>header</p>
-            {props.markup}
+            {props.children}
             <p>footer</p>
         </div>
     )
 };
 
-const template = (
-    <div>
-        <h1>Page Title</h1>
-        <p>This is my page</p>
-    </div>
-);
+// const template = (
+//     <div>
+//         <h1>Page Title</h1>
+//         <p>This is my page</p>
+//     </div>
+// );
 
-ReactDOM.render(<Layout markup={template}/>, document.getElementById('app'));
+ReactDOM.render(
+    <Layout>
+        <div>
+            <h1>Page Title</h1>
+            <p>This is my page</p>
+        </div>
+    </Layout>
+    , document.getElementById('app'));
