56ec238aa5716c12a61d115ebc802b7435658228
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-12-30 10:45:41 +0000

    Widget SCSS added - background/color/padding

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,11 +4,14 @@ import Option from './Option'
 
 const Options = (props) => (
   <div>
-    <button 
-    className="button--link"
-    onClick={props.handleRemoveAllOptions}>
-    Remove All
+    <div className="widget-header">
+      <h3>Your Options</h3>
+      <button
+        className="button--link"
+        onClick={props.handleRemoveAllOptions}>
+        Remove All
     </button>
+    </div>
     {props.options.map((option) => <Option key={option} optionText={option} handleRemoveOption={props.handleRemoveOption} />)}
   </div>
 );
