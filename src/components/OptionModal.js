63e1658d89983d348a7dcd8fcc0c1497aca171a9
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-04 13:02:55 +0000

    Stlyling - Modal form fade in/out

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -4,8 +4,9 @@ import Modal from 'react-modal';
 const OptionModal = (props) => (
 <Modal
     isOpen={!!props.selectedOption}
-    onRequestClose={props.closeModal} // allows close by Esc or clicking outside the modal
-    contentLabel="Selected Option"  // required attribute (accessibility)
+    onRequestClose={props.closeModal}   // allows close by Esc or clicking outside the modal
+    contentLabel="Selected Option"      // required attribute (accessibility)
+    closeTimeoutMS={200}                // delays the removal of the item from DOM
 >
     <h3>Selected Option</h3>
     {props.selectedOption && <p>{props.selectedOption}</p>}
