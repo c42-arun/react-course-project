52e1158106b0c3faaf4b009312680979ef784174
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-04 13:43:56 +0000

    Styling - modal contents

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,11 @@ const OptionModal = (props) => (
     onRequestClose={props.closeModal}   // allows close by Esc or clicking outside the modal
     contentLabel="Selected Option"      // required attribute (accessibility)
     closeTimeoutMS={200}                // delays the removal of the item from DOM
+    className="modal"                   // overrides the styles applied 
 >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.closeModal}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.closeModal}>Okay</button>
 </Modal>
 );
 
